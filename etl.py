a3eac063a631c73d27457fa578b7bbcde1172883
MariaVitoriaK <197960@upf.br>
2026-03-27 09:38:27 -0300

    arrumado o etl e add o readme

--- etl.py
+++ etl.py
@@ -3,8 +3,12 @@ import glob
 from sqlalchemy import create_engine
 
 # conexão com banco
-engine = create_engine("postgresql://usuario:senha@localhost:5432/dw_cartao")
+engine = create_engine("postgresql://postgres:Melissak12.@localhost/dw_cartao")
 
+print("Testando conexão...")
+with engine.connect() as conn:
+    print("Conectado com sucesso!")
+    
 arquivos = glob.glob("dados/Fatura_*.csv")
 
 df_total = []
@@ -46,21 +50,40 @@ def trata_parcela(x):
 df[['num_parcela','total_parcelas']] = df['Parcela'].apply(lambda x: pd.Series(trata_parcela(x)))
 
 # =========================
-# LOAD DIMENSÕES
+# CRIA DIMENSÕES
 # =========================
 
 dim_data = df[['Data de Compra','dia','mes','trimestre','ano','dia_semana']].drop_duplicates()
 dim_data.columns = ['data','dia','mes','trimestre','ano','dia_semana']
-dim_data.to_sql("dim_data", engine, if_exists="append", index=False)
 
 dim_titular = df[['Nome no Cartão','Final do Cartão']].drop_duplicates()
 dim_titular.columns = ['nome_titular','final_cartao']
-dim_titular.to_sql("dim_titular", engine, if_exists="append", index=False)
 
 dim_categoria = df[['Categoria']].drop_duplicates()
 dim_categoria.columns = ['nome_categoria']
-dim_categoria.to_sql("dim_categoria", engine, if_exists="append", index=False)
 
 dim_est = df[['Descrição']].drop_duplicates()
 dim_est.columns = ['nome_estabelecimento']
-dim_est.to_sql("dim_estabelecimento", engine, if_exists="append", index=False)
+
+
+# =========================
+# LOAD NO BANCO
+# =========================
+
+try:
+    dim_data.to_sql("dim_data", engine, if_exists="append", index=False)
+    print("dim_data OK")
+
+    dim_titular.to_sql("dim_titular", engine, if_exists="append", index=False)
+    print("dim_titular OK")
+
+    dim_categoria.to_sql("dim_categoria", engine, if_exists="append", index=False)
+    print("dim_categoria OK")
+
+    dim_est.to_sql("dim_estabelecimento", engine, if_exists="append", index=False)
+    print("dim_est OK")
+
+except Exception as e:
+    print("ERRO AO INSERIR:", e)
+
+print("Dados carregados com sucesso!")
